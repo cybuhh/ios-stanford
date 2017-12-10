685e083f213f06c9494feb1b84fcd7386aa70f01
cybuhh <>
2017-12-10 23:54:45 +0100

    models

---
+++ stanford/Card.swift
@@ -0,0 +1,26 @@
+//
+//  Card.swift
+//  stanford
+//
+//  Created by cybuhh on 10/12/2017.
+//  Copyright © 2017 cybuhh. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1;
+        return identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
--- stanford/ViewController.swift
+++ stanford/ViewController.swift
@@ -9,33 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var flipCountLabel: UILabel!
-    @IBOutlet var cardButtons: [UIButton]!
-    
-    var labels = ["🤪", "😎", "🤪", "😎"];
+    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+
     var flipCount = 0 {
         didSet {
             flipCountLabel.text = "Flip count: \(flipCount)"
         }
     }
-
-    func flipButton(withEmoji emoji: String, on sender: UIButton) {
-        if (sender.currentTitle == "") {
-            sender.setTitle(emoji, for: UIControlState.normal)
-            sender.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        } else {
-            sender.setTitle("", for: UIControlState.normal)
-            sender.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-        }
-    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
     
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1;
         if let cardNumber = cardButtons.index(of: sender) {
-            flipButton(withEmoji: labels[cardNumber], on: sender)
+            game.choosedCard(at: cardNumber)
+            updateViewFromModel()
         } else {
             print("choosen card not found in array")
+        }
+    }
+    
+    var emojiChoices = ["🤪", "😎", "😱", "🤯", "🤬"];
+
+    var emoji = [Int:String]()
+    
+    func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        
+        return emoji[card.identifier] ?? "?"
+    }
+    
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+            } else {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            }
         }
     }
 }

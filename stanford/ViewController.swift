33f3dd43fbb08213bc1dd1557f701a7fd0e54bc7
cybuhh <>
2017-12-10 23:04:12 +0100

    initial

--- stanford/ViewController.swift
+++ stanford/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var labels = ["🤪", "😎", "🤪", "😎"];
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flip count: \(flipCount)"
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func flipButton(withEmoji emoji: String, on sender: UIButton) {
+        if (sender.currentTitle == "") {
+            sender.setTitle(emoji, for: UIControlState.normal)
+            sender.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        } else {
+            sender.setTitle("", for: UIControlState.normal)
+            sender.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        }
     }
-
-
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1;
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipButton(withEmoji: labels[cardNumber], on: sender)
+        } else {
+            print("choosen card not found in array")
+        }
+    }
 }
 

64f6424aa8243d2c068ba9e1458f55cc4130d101
cybuhh <>
2017-12-14 11:46:17 +0100

    Int extension

--- stanford/ViewController.swift
+++ stanford/ViewController.swift
@@ -37,8 +37,7 @@ class ViewController: UIViewController {
     
     private func emoji(for card: Card) -> String {
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
         }
         
         return emoji[card.identifier] ?? "?"
@@ -59,3 +58,13 @@ class ViewController: UIViewController {
     }
 }
 
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)));
+        } else if self < 0 {
+            return Int(arc4random_uniform(UInt32(abs(self))));
+        }
+        return 0
+    }
+}

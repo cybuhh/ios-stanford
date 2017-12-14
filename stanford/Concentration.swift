6e1f54462a13e5940a56d6a4d7ea855f559bb812
cybuhh <>
2017-12-14 11:45:22 +0100

    asserts

--- stanford/Concentration.swift
+++ stanford/Concentration.swift
@@ -34,6 +34,7 @@ class Concentration {
     }
 
     func choosedCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): chosen index not in the cards")
         if !cards[index].isMatched {
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
                 // check if cards match
@@ -50,6 +51,7 @@ class Concentration {
     }
     
     init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards): you must have at least one pair of cards")
         for _ in 0..<numberOfPairsOfCards {
             let card = Card()
             cards += [card, card]
